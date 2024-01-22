36215722a2a92663a3ce446669c2195279adc3fe
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2024-01-21 21:25:12 -0800

    added class 24

--- Class 23/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ Class 23/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,17 @@
-//Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a Tony Hawk Pro Skater constructor that makes skating game characters with 4 properties and 3 methods
+
+function MakeSkater(skaterSponsor, skaterLeadFoot, skaterSpeed, skaterHealth) {
+  this.sponsor = skaterSponsor;
+  this.leadFoot = skaterLeadFoot;
+  this.speed = skaterSpeed;
+  this.health = skaterHealth;
+  this.trick = () => {
+    console.log("KICK FLIP BABY");
+  };
+  this.jump = () => {
+    console.log("GETTING AIR");
+  };
+  this.getUp = () => {
+    console.log("GETTING UP BAYBEEEE");
+  };
+}
