09bc6e93294c18645a7b06469b769205026e9ceb
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2024-02-19 23:44:20 -0800

    finished first project

--- Class 26/homework/brewery/js/main.js
+++ Class 26/homework/brewery/js/main.js
@@ -4,8 +4,8 @@ document.querySelector("button").addEventListener("click", function () {
 });
 
 function getFetch(city) {
-  let formatedCity = city.toLowerCase().replace(" ", "&20");
-  let url = `https://api.openbrewerydb.org/v1/breweries?by_city=${formatedCity}&per_page=3`;
+  let formatedCity = city.toLowerCase().replace(" ", "_");
+  let url = `https://api.openbrewerydb.org/v1/breweries?by_city=${formatedCity}&per_page=50`;
 
   fetch(url)
     .then((res) => res.json())
@@ -21,23 +21,43 @@ function displayBreweries(breweries) {
   const breweryList = document.getElementById("breweryList");
   breweryList.innerHTML = "";
 
-  breweryList.forEach((brewery) => {
+  for (let i = 0; i < breweries.length; i++) {
     const breweryElement = document.createElement("div");
-    breweryElement.classList.add("brewery");
-
     const name = document.createElement("h2");
-    name.textContent = brewery.name;
+    name.textContent = breweries[i].name;
+    const address = document.createElement("p");
+    if (breweries[i].address_1 === null) {
+      address.textContent = `${breweries[i].city} ${breweries[i].postal_code}`;
+    } else {
+      address.textContent = `${breweries[i].address_1}, ${breweries[i].city} ${breweries[i].postal_code}`;
+    }
 
-    const type = document.createElement("p");
-    type.textContent = `Type: ${brewery.type}`;
-
-    const city = document.createElement("p");
-    city.textContent = `City: ${brewery.city}`;
+    const website = document.createElement("p");
+    website.textContent = breweries[i].website_url;
 
     breweryElement.appendChild(name);
-    breweryElement.appendChild(type);
-    breweryElement.appendChild(city);
-
+    breweryElement.appendChild(address);
+    breweryElement.appendChild(website);
     breweryList.appendChild(breweryElement);
-  });
+  }
 }
+
+// breweries.forEach((brewery, i) => {
+//   const breweryElement = document.createElement("div");
+//   breweryElement.classList.add("brewery");
+
+//   const name = document.createElement("h2");
+//   name.textContent = brewery[i].name;
+
+//   const type = document.createElement("p");
+//   type.textContent = `Type: ${brewery[i].type}`;
+
+//   const city = document.createElement("p");
+//   city.textContent = `City: ${brewery[i].city}`;
+
+//   breweryElement.appendChild(name);
+//   breweryElement.appendChild(type);
+//   breweryElement.appendChild(city);
+
+//   breweryList.appendChild(breweryElement);
+// });
